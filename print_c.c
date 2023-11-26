113aec0a7bd72df14db3e98da0a274bf6e5fc9d9
cel3788 <146735843+cel3788@users.noreply.github.com>
2023-11-26 16:04:30 +0100

    Update print_c.c
    In this commit:
    - Implement the _function_type function to handle various format specifiers in the _printf implementation
    - Utilize an array of structures to map format specifiers to corresponding printing functions
    - Call the appropriate printing function based on the format specifier provided
    - Handle the case where the format specifier is not recognized by printing "%<specifier>" to indicate an unsupported specifier
    
    This commit enhances the functionality of the _printf function by implementing the logic to handle different format specifiers.

--- print_c.c
+++ print_c.c
@@ -1,16 +1,41 @@
 #include "main.h"
 
 /**
- *  * print_c - prints a character.
- *   * @val: arguments.
- *    * Return: integer.
- *     */
-int print_c(va_list val)
+ 
+_function_type - check the code
+@c: char
+@avar: va_list
+Return: Number of character print
+*/
+int _function_type(char c, va_list avar)
 {
-	char s;
+        formats list[] = {
+                {"c", _print_char},
+                {"s", _print_arg_string},
+                {"d", _print_int},
+                {"i", _print_int},
+                {NULL, NULL}
+        };
 
-	s = va_arg(val, int);
-	_putchar(s);
-	return (1);
-}
+        int i = 0;
+
+        while (list[i].t != NULL)
+        {
+                if (*list[i].t == c)
+                {
+                        return (list[i].f(avar));
+                }
+                i++;
+        }
+
+        if (c == '%')
+        {
+                return (_putchar('%'));
+        }
+        else
+        {
+                _putchar('%');
+                _putchar(c);
+                return (2);
+        }
 
