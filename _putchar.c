973e570a2eacaae64eaf4db9f249b63b2cbac1db
cel3788 <7576@holbertonstudents.com>
2023-11-26 09:36:15 -0800

    _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,5 @@
 #include "MAIN.H"
-#include <unistd.h> // Ajout de l'inclusion du fichier d'en-tête pour 'write'
+#include <unistd.h> /* Ajout de l'inclusion du fichier d'en-tête pour 'write' */
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,5 +10,6 @@
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1); // Utilisation de la fonction write sans parenthèses inutiles
+    return write(1, &c, 1); /* Utilisation de la fonction write sans parenthèses inutiles */
 }
+
