3106ca78673e5e427db78df2866369649d30cc24
cel3788 <7576@holbertonstudents.com>
2023-11-26 10:37:56 -0800

    printf.c

--- printf.c
+++ printf.c
@@ -1,50 +1,30 @@
-#include <stdarg.h>
-#include <string.h>
 #include "main.h"
 
 /**
-
-  _printf - function to print a string formated
-  @format: string of format
+ * _printf - Produces output according to a format
+ * @format: Is a character string. The format string
+ * is composed of zero or more directives
  *
-Return: the total number of the caracter print
-*/
-
-int _printf(const char format, ...)
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
+int _printf(const char *format, ...)
 {
-	va_list avar;
-	int totalLen = 0;
+	int size;
+	va_list args;
 
+	if (format == NULL)
+		return (-1);
 
-	if (!format || (strlen(format) == 1 && format[0] == '%'))
-		/Vérifier si la chaîne de format est NULL/
-			return (-1);
+	size = _strlen(format);
+	if (size <= 0)
+		return (0);
 
-	va_start(avar, format);
+	va_start(args, format);
+	size = handler(format, args);
 
-	while (format)
-		/Boucle pour parcourir chaque caractère de format/
-		{
+	_putchar(-1);
+	va_end(args);
 
-			if (format == '%')
-				/Vérifier si le caractère actuel est un '%'/
-				{
-					format++;
-					if (format == '\0')
-						break;
-					totalLen += _function_type(format, avar);
-					/Apl la fonction _function_type pour gérer le spécificateur de format/
-				}
-			else
-			{
-				_putchar(format);
-				/Appeler directement _putchar pour imprimer le caractère/
-					totalLen++;
-			}
-			format++;
-		}
-
-	va_end(avar);
-
-	return (totalLen);
-	/Retourner le nombre total de caractères imprimés/
+	return (size);
+}
