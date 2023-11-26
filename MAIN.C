826671faf59aede36b7ca507d0975bca51e98f85
cel3788 <7576@holbertonstudents.com>
2023-11-26 09:49:24 -0800

    add MAIN.C

--- MAIN.C
+++ MAIN.C
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0
  */
-int _putchar(char c)
+int main(void)
 {
-    return putchar(c);
+    /* Your test code here */
+    return (0);
 }
-
