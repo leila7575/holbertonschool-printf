8ba09e4167d1e7574a1ca6a4f81ccd8d2514d8a0
cel3788 <146735843+cel3788@users.noreply.github.com>
2023-11-26 15:41:33 +0100

    Update main.h
    In this commit:
    - Refactor code in _printf.c and FUNCTION_TYPE.c
    - Update the name of the structure field in FUNCTION_TYPE.c from "t" to "c" for clarity
    - Update the name of the variable "totallen" to "leila" in FUNCTION_TYPE.c for consistency and clarity
    - Add missing comments to enhance code documentation
    - Fix the formatting issue in the header guard of main.h
    - Improve comments for better readability
    
    These changes contribute to the overall readability and maintainability of the codebase.

--- main.h
+++ main.h
@@ -1,45 +1,120 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <limits.h>
+#ifndef MAIN
+#define MAIN
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char format, ...);
+int _print_arg_string(va_list avar);
+int _print_string(chars);
+int _print_char(va_list avar);
+int _function_type(char c, va_list avar);
+int _print_int(va_list arg);
+int _putchar(char c);
+/**
+ 
+struct formats - Struct dog*
+@t: first letter of the type
+@f: function execute per type
+*/
+typedef struct formats
+{
+        char t;
+        int (f)(va_list p);
+} formats;
+#endif
+FUNCTION_TYPE.C
+#include "main.h"
 
 /**
- * struct format - converter for printf
- * @ph: type char pointer of the specifier
- * @function: function for the conversion specifier
- */
-
-typedef struct format
+ 
+_function_type - check the code
+@c: char
+@avar: va_list
+Return: Number of character print
+*/
+int _function_type(char c, va_list avar)
 {
-	char *ph;
-	int (*function)();
-} convert;
+        formats list[] = {
+                {"c", _print_char},
+                {"s", _print_arg_string},
+                {"d", _print_int},
+                {"i", _print_int},
+                {NULL, NULL}
+        };
 
-int *_strcpy(char *dest, char *src);
-int print_pointer(va_list val);
-int print_unsigned(va_list args);
-int print_hex_extra(unsigned long int num);
-int _strlenc(const char *s);
-int print_HEX_extra(unsigned int num);
-int print_exc_string(va_list val);
-int print_HEX(va_list val);
-int print_hex(va_list val);
-int print_oct(va_list val);
-int print_bin(va_list val);
-int print_revs(va_list args);
-int print_rot13(va_list args);
-int print_i(va_list args);
-int print_d(va_list args);
-int _strlen(char *s);
-int rev_string(char *s);
-int print_37(void);
-int print_c(va_list val);
-int print_s(va_list val);
-int _putchar(char c);
-int _printf(const char *format, ...);
+        int i = 0;
 
-#endif
+        while (list[i].t != NULL)
+        {
+                if (*list[i].t == c)
+                {
+                        return (list[i].f(avar));
+                }
+                i++;
+        }
+
+        if (c == '%')
+        {
+                return (_putchar('%'));
+        }
+        else
+        {
+                _putchar('%');
+                _putchar(c);
+                return (2);
+        }
+_PRINTF.C
+#include <stdarg.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ 
+_printf - function to print a string formated
+@format: string of format
+*
+Return: the total number of the caracter print
+*/
+
+int _printf(const char format, ...)
+{
+        va_list avar;
+        int totalLen = 0;
+
+
+        if (!format || (strlen(format) == 1 && format[0] == '%'))
+                /Vérifier si la chaîne de format est NULL/
+                return (-1);
+
+        va_start(avar, format);
+
+        while (format)
+                /Boucle pour parcourir chaque caractère de format/
+        {
+
+                if (format == '%')
+                        /Vérifier si le caractère actuel est un '%'/
+                {
+                        format++;
+                        if (format == '\0')
+                                break;
+                        totalLen += _function_type(format, avar);
+                        /Apl la fonction _function_type pour gérer le spécificateur de format/
+                }
+                else
+                {
+                        _putchar(format);
+                        /Appeler directement _putchar pour imprimer le caractère/
+                        totalLen++;
+                }
+                format++;
+        }
+
+        va_end(avar);
+
+        return (totalLen);
+        /Retourner le nombre total de caractères imprimés/
 
